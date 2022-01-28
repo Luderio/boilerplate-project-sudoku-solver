a40c072a35dc34596941b70248d501bb08262ec3
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-28 09:28:39 +0800

    unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,14 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
 
-suite('UnitTests', () => {
-
+suite('UnitTests', function() {
+    suite('Routing Tests', function() {
+        
+        //Test 1: Logic handles a valid puzzle string of 81 characters.
+        test('String should be 81 characters long', function(done) {
+            assert.equal(solver.solve.length, 81);
+        });
+    });
 });
