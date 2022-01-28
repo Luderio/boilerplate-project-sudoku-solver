77eb9ff349ae705b92e4fbb2cba1b0ac54646730
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-28 11:40:04 +0800

    api.js 'api/check' route updated

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,44 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      let puzzleString = req.body.puzzle;
+      let coordinate = req.body.coordinate;
+      let value = req.body.value;
 
+      solver.validate(puzzleString);
+      let validate = solver.validate(puzzleString);
+
+      let validCoordinate = [];//contains valid coordinates.
+
+      let len = 81;
+      for (let i = 0; i < len; i++) {
+        let rowLetter = String.fromCharCode('A'.charCodeAt(0) + Math.floor(i / 9));
+        let col = (i % 9) + 1; 
+        validCoordinate.push(rowLetter + col);
+      }
+
+      if (!validCoordinate.includes(coordinate)) {
+        return res.json({ error: 'Invalid coordinate'});
+      }
+
+      if (value < 1 || value > 9) {
+        return res.json({ error: 'Invalid value' });
+      }
+
+
+      if (validate == 'Invalid characters in puzzle') {
+        return res.json({error: 'Invalid characters in puzzle'});
+      }
+
+      if (validate == 'Expected puzzle to be 81 characters long') {
+        return res.json({error: 'Expected puzzle to be 81 characters long'});
+      }
+
+      if (validate == 'Required field missing' || coordinate == '' || value == '') {
+        return res.json({error: 'Required field(s) missing'});
+      }
+
+      
     });
     
     app.route('/api/solve')
