0db05574484364d7ee5b4106eacbc0da38d71145
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-28 09:29:06 +0800

    unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,6 +10,7 @@ suite('UnitTests', function() {
         //Test 1: Logic handles a valid puzzle string of 81 characters.
         test('String should be 81 characters long', function(done) {
             assert.equal(solver.solve.length, 81);
+            done();
         });
     });
 });
