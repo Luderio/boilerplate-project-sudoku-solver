b188ddfdefb69a3a8d0f7172eab7e3f9b7f94f86
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-29 16:26:51 +0800

    Tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,9 +10,9 @@ suite('UnitTests', function() {
         
         //Test 1: Logic handles a valid puzzle string of 81 characters.
         test('String should be 81 characters long', function(done) {
-            let inputString = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-            assert.equal(solver.solve(inputString).length, 81);
-            assert.equal(solver.validate(inputString), true);
+            let inputString = '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+            let completeSolution = '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
+            assert.equal(solver.solve(inputString), completeSolution);
             done();
         });
 
